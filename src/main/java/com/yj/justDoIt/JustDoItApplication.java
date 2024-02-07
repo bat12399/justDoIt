69e176de1448c09ec032daa754022bb0938485f0
ouj <ouj@excellence.com.cn>
2024-02-07 17:27:04 +0800

    项目创建

---
+++ src/main/java/com/yj/justDoIt/JustDoItApplication.java
@@ -0,0 +1,14 @@
+package com.yj.justDoIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class JustDoItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustDoItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yj/justDoIt/JustDoItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yj.justDoIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustDoItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
